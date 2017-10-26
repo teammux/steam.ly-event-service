24497f3300be220e0632041531815f235ae9eb44
Pegasus <pegasus@yunio.com>
2017-10-26 12:06:27 -0700

    add test

---
+++ test/modelSpec.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const model = require('../database/model.js');
+
+describe('model', function() {
+  describe('dateCheck', function() {
+
+    it('true', function() {
+      assert.equal(1,1);
+    });
+
+  });
+
+});
